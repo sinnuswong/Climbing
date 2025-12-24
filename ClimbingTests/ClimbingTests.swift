1e4f5196957086c5f690026feca3a1df7d7040bb
Sinnus <sinnus@qq.com>
2025-12-24 15:23:18 +0800

    Initial Commit

---
+++ ClimbingTests/ClimbingTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClimbingTests.swift
+//  ClimbingTests
+//
+//  Created by Sinnus Wong on 12/24/25.
+//
+
+import Testing
+
+struct ClimbingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
